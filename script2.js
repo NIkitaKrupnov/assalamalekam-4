d43ceb492e65954e516a9cd5d7f492187dcc9aca
TurquoiseOSU <coffeecream2005@mail.ru>
2019-09-25 13:45:04 +0300

    first commit

---
+++ script2.js
@@ -0,0 +1,12 @@
+let num1=prompt("введите первое число")
+let num2=prompt("введите второе число")
+let num3=prompt("введите третье число")
+if (num1>num2 && num1>num3) {
+alert(num1)
+} else if (num2>num1 && num2>num3) {
+alert(num2)
+}else {
+  alert(num3);
+}{
+
+}
---
+++ script3.js
@@ -0,0 +1,8 @@
+let num1=prompt("введите число а")
+let num2=prompt("введите число b")
+let num3=confirm("выполнить сложение?")
+if (num3===true) {alert(num1+num2);
+
+} else {alert("ассаламалейкум брат");
+
+}
